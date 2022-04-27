2717bc29a8f07841ac96fc8db1bc2d262b935212
aliakseip <aliakseip@playtika.com>
2022-04-27 23:41:08 +0300

    Added letter-box

---
+++ Wordle/Models/Letter/LetterBox.swift
@@ -0,0 +1,13 @@
+//
+//  LetterBox.swift
+//  Wordle
+//
+//  Created by Aliaksei Palianskikh on 27.04.22.
+//
+
+import Foundation
+
+struct LetterBox {
+    let letter : String
+    let status : LetterEvaluation?
+}
